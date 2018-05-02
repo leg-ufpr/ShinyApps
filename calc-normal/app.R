593f28ffb8ee7d0a6815b25318f2be087e0cf11c
Hektor Dannyel <hektor.brasil@gmail.com>
2018-05-01 21:56:52 -0300

    Adiciona distribuição t
    Adiciona aba com a calculadora da distribuição t-Student. Altera também o posicionamento do valor no gráfico, mantendo sempre centralizado.

--- calc-normal/app.R
+++ calc-normal/app.R
@@ -1,11 +1,9 @@
 library(shiny)
 library(scales)
 
-options(scipen=1)
-
 ui <- navbarPage(
   
-  "Distribuição Normal",
+  "Calculadoras de Distribuições de Probabilidade Contínuas",
   
   tabPanel(
     "Calculadora da Distribuição Normal",
@@ -54,7 +52,7 @@ ui <- navbarPage(
 ),
 
   tabPanel(
-    "Calculadora da Distribuição Normal Quantilica",
+    "Calculadora de quantis da Distribuição Normal",
     
     column(3, wellPanel(
       
@@ -93,9 +91,53 @@ ui <- navbarPage(
       plotOutput("plot_q")
       
     ))
+  ),
+
+  tabPanel(
+    
+    "Distribuição t-Student",
+    
+    column(3, wellPanel(
+      
+      numericInput(
+        inputId = "x_t_lower",
+        label = "Limite inferior de x",
+        value = -5
+      ),
+      
+      numericInput(
+        inputId = "x_t_upper",
+        label = "Limite superior de x",
+        value = 5
+      ),
+      
+      numericInput(
+        inputId = "df_t",
+        label = "Graus de liberdade",
+        value = 5,
+        min = 2
+      ),
+      
+      verbatimTextOutput("t"),
+      
+      submitButton(
+        text = "Calcular!"
+      )
+      
+    )),
+      
+      column(9, wellPanel(
+        
+        plotOutput("plot_t")
+        
+      ))
+    
+    
   )
 
 )
+
+
 
 server <- function(input, output){
   
@@ -112,21 +154,21 @@ server <- function(input, output){
       if(is.finite(input$xlower)){
         lower <- input$xlower
         upper <- input$mu + 4*input$sd
-        x <- seq(from = lower, to = upper, length.out = 10000)
+        x <- seq(from = lower, to = upper, length.out = 1000)
         col <- "red"
         border <- "black"
       }
       else if(is.finite(input$xupper)){
         lower <- input$mu - 4*input$sd
         upper <- input$xupper
-        x <- seq(from = lower, to = upper, length.out = 10000)
+        x <- seq(from = lower, to = upper, length.out = 1000)
         col <- "red"
         border <- "black"
       }
       else if(!is.finite(input$xlower) & !is.finite(input$xlower)){
         lower <- input$mu - 4*input$sd
         upper <- input$mu + 4*input$sd
-        x <- seq(from = lower, to = upper, length.out = 10000)
+        x <- seq(from = lower, to = upper, length.out = 1000)
         col = NA
         border = NA
       }
@@ -138,36 +180,16 @@ server <- function(input, output){
     xcoord <- c(lower, x, upper)
     ycoord <- c(0, y, 0)
     
-    curve(dnorm(x, input$mu, input$sd), xlim = c(input$mu - 4*input$sd, input$mu + 4*input$sd), ylab = "Probabilidade")
+    crv_norm <- curve(dnorm(x, input$mu, input$sd), xlim = c(input$mu - 4*input$sd, input$mu + 4*input$sd), ylab = "Probabilidade")
     
     polygon(x = xcoord,
             y = ycoord,
             col = col, border = border)
     
     text(x = input$mu,
-         y = median(ycoord),
+         y = max(crv_norm$y)/2,
          labels = round((pnorm(upper, input$mu, input$sd) -
            pnorm(lower, input$mu, input$sd)), 4),
-         cex = 4)
-    
-  })
-  
-  output$plot_q <- renderPlot({
-    
-    curve(dnorm(x, input$mu_q, input$sd_q), xlim = c(input$mu_q - 4*input$sd_q, input$mu_q + 4*input$sd_q), ylab = "Probabilidade")
-    
-    y_q <- qnorm(input$q, input$mu_q, input$sd_q)
-    x_q <- seq(from = input$mu_q - 4*input$sd_q, to = y_q, length.out = 1000)
-    y_q.coord <- dnorm(x_q, input$mu_q, input$sd_q)
-    
-    
-    polygon(x = c(input$mu_q - 4*input$sd_q, x_q, y_q),
-            y = c(0, y_q.coord, 0),
-            col = "red", border = "black")
-    
-    text(x = input$mu_q,
-         y = dnorm(input$mu_q)/2,
-         labels = round(y_q),
          cex = 4)
     
   })
@@ -197,9 +219,58 @@ server <- function(input, output){
     
   })
   
+  output$plot_q <- renderPlot({
+    
+    crv_q <- curve(dnorm(x, input$mu_q, input$sd_q), xlim = c(input$mu_q - 4*input$sd_q, input$mu_q + 4*input$sd_q), ylab = "Probabilidade")
+    
+    y_q <- qnorm(input$q, input$mu_q, input$sd_q)
+    x_q <- seq(from = input$mu_q - 4*input$sd_q, to = y_q, length.out = 1000)
+    y_q.coord <- dnorm(x_q, input$mu_q, input$sd_q)
+    
+    
+    polygon(x = c(input$mu_q - 4*input$sd_q, x_q, y_q),
+            y = c(0, y_q.coord, 0),
+            col = "red", border = "black")
+    
+    text(x = input$mu_q,
+         y = max(crv_q$y)/2,
+         labels = round(y_q),
+         cex = 4)
+    
+  })
+  
   output$norm_q <- renderPrint({
     
     qnorm(input$q, input$mu_q, input$sd_q)
+    
+  })
+  
+  output$plot_t <- renderPlot({
+    
+    crv_t <- curve(dt(x, input$df_t), xlim = c(-4 * input$df_t/(input$df_t - 2), 4 * input$df_t/(input$df_t - 2)), ylab = "Probabilidade")
+    
+    x <- seq(from = input$x_t_lower, to = input$x_t_upper, length.out = 1000)
+    
+    y <- dt(x, input$df_t)
+    
+    xcoord <- c(input$x_t_lower, x, input$x_t_upper)
+    ycoord <- c(0, y, 0)
+    
+    polygon(x = xcoord,
+            y = ycoord,
+            col = "red", border = "black")
+    
+    text(x = 0,
+         y = max(crv_t$y)/2,
+         labels = round((pt(input$x_t_upper, input$df_t) -
+                           pt(input$x_t_lower, input$df_t)), 4),
+         cex = 4)
+    
+  })
+  
+  output$t <- renderPrint({
+    
+    pt(input$x_t_upper, input$df_t) - pt(input$x_t_lower, input$df_t)
     
   })
   
