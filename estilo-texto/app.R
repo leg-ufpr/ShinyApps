81b1c76279582ffd69b6878c1af13aaa20e4a973
Walmes Zeviani <walmes@ufpr.br>
2019-09-02 14:17:22 -0300

    Improves code.

--- estilo-texto/app.R
+++ estilo-texto/app.R
@@ -15,6 +15,11 @@
 
 rm(list = ls())
 
+# DANGER: remove os arquivos de log.
+# unlink("counter.txt")
+# file.rename(from = "output.txt",
+#             to = sprintf("output-%d.txt", as.integer(Sys.time())))
+
 #-----------------------------------------------------------------------
 # Carrega pacotes necessários.
 
@@ -30,6 +35,9 @@ smpread <- sample(list.files(pattern = "texto.*.txt")[1:4],
 # Importa o texto.
 toread <- readLines(con = smpread)
 
+# # Usa um único texto.
+# toread <- readLines(con = "texto1.txt")
+
 #-----------------------------------------------------------------------
 # Define argumentos do estilo do texto.
 
@@ -40,48 +48,84 @@ style_list <- list("font-family" = c("arial", "times"),
                    "width" = c("300px", "800px"),
                    "color" = c("black", "#a4a4a4"))
 
-# style_vec <- sapply(style_list, FUN = sample, size = 1)
+# ATTENTION! Mais opções para extender a aplicação:
+#   background: {blue, red};
+#   font-weight: {normal, bold};
+#   letter-spacing: {-0.05cm, 5px};
+
+# #-----------------------------------------------------------------------
+# # Lê arquivo com realizações anteriores para ponderar a escolha do ponto
+# # experimental a ser apresentado para o usuário.
+#
+# # Todas as combinações entre os níveis.
+# all_cells <- do.call(expand.grid, args = style_list)
+# attr(all_cells, "out.attrs") <- NULL
+#
+# # Prepara tabela conforme existência de informação prévia.
+# if (file.exists("output.txt")) {
+#     cells <- read.table("output.txt", header = FALSE, sep = ";")
+#     cells <- cells[, 3:(2 + ncol(all_cells))]
+#     names(cells) <- names(all_cells)
+# } else {
+#     cells <- all_cells[0, ]
+# }
+#
+# # Concatena os pontos experimentais disponíveis com os realizados.
+# done_cells <- rbind(all_cells, cells)
+#
+# # Determina a frequência de cada ponto experimental.
+# cell_freq <- as.data.frame(xtabs(~ ., done_cells))
+# names(cell_freq)[-ncol(cell_freq)] <- names(all_cells)
+#
+# #-----------------------------------------------------------------------
+# # Faz o sorteio do ponto experimental com prioridade inversa a
+# # frequência observada.
+#
+# # Favoresce o sorteio de pontos experimentais menos frequentes.
+# i <- sample(1:nrow(cell_freq),
+#             size = 1,
+#             prob = 1/(cell_freq$Freq))
+#
+# # Cria o vetor de estilos.
+# style_vec <- unlist(as.list(cell_freq[i, names(style_list)]))
 # style <- sprintf("%s: %s;",
 #                  names(style_vec),
 #                  style_vec)
 # style <- paste(style, collapse = " ")
 # style
+#
+# # Adiciona propriedades fixas.
+# style <- paste(style, "margin: 2em;")
 
 #-----------------------------------------------------------------------
-# Lê arquivo com realizações anteriores para ponderar a escolha do ponto
-# experimental a ser apresentado para o usuário.
+
+# Se arquivo com contagem não existir, então criar.
+if (!file.exists("counter.txt")) {
+    cat(0, "\n", file = "counter.txt")
+}
+cnt <- as.integer(readLines("counter.txt"))
 
 # Todas as combinações entre os níveis.
 all_cells <- do.call(expand.grid, args = style_list)
 attr(all_cells, "out.attrs") <- NULL
 
-# Prepara tabela conforme existência de informação prévia.
-if (file.exists("output.txt")) {
-    cells <- read.table("output.txt", header = FALSE, sep = ";")
-    cells <- cells[, 3:(2 + ncol(all_cells))]
-    names(cells) <- names(all_cells)
-} else {
-    cells <- all_cells[0, ]
-}
+# Numeração dos pontos experimentais.
+pp <- 1:nrow(all_cells)
 
-# Concatena os pontos experimentais disponíveis com os realizados.
-done_cells <- rbind(all_cells, cells)
+# Ordem de execução dos pontos experimentais.
+set.seed(123)
+u <- sample(pp)
 
-# Determina a frequência de cada ponto experimental.
-cell_freq <- as.data.frame(xtabs(~ ., done_cells))
-names(cell_freq)[-ncol(cell_freq)] <- names(all_cells)
+# Determina a ordem da corrida experimental que será feita.
+cnt <- cnt + 1
+i <-  cnt %% nrow(all_cells)
 
-#-----------------------------------------------------------------------
-# Faz o sorteio do ponto experimental com prioridade inversa a
-# frequência observada.
-
-# Favoresce o sorteio de pontos experimentais menos frequentes.
-i <- sample(1:nrow(cell_freq),
-            size = 1,
-            prob = 1/(cell_freq$Freq))
+u[i] # Ponto experimental.
 
 # Cria o vetor de estilos.
-style_vec <- unlist(as.list(cell_freq[i, names(style_list)]))
+style_vec <- unlist(as.list(all_cells[u[i], names(style_list)]))
+style_vec <- setNames(as.character(style_vec),
+                      names(style_vec))
 style <- sprintf("%s: %s;",
                  names(style_vec),
                  style_vec)
@@ -94,6 +138,9 @@ style <- paste(style, "margin: 2em;")
 #-----------------------------------------------------------------------
 # Frontend.
 
+credits <- paste("<br><p>Desenvolvido por <a href='https://github.com/vriffel'>Vinicius Riffel</a>.",
+                 "Código fonte disponível em <a href='https://github.com/leg-ufpr/ShinyApps/tree/master/estilo-texto'>github.com/leg-ufpr/ShinyApps</a>.</p>")
+
 ui <- fluidPage(
     includeHTML("../GLOBAL/header.html"),
     includeHTML("../GLOBAL/MathJax.html"),
@@ -105,10 +152,23 @@ ui <- fluidPage(
         hr(),
         textOutput("text"),
         hr(),
-        actionButton("start", label = "Começar"),
-        actionButton("end", label = "Terminar"),
+        actionButton("start",
+                     label = "Começar",
+                     width = "10em",
+                     icon = icon("play"),
+                     class = "btn btn-primary"),
+        actionButton("submit",
+                     label = "Submeter",
+                     width = "10em",
+                     icon = icon("check-square"),
+                     class = "btn btn-success"),
+        actionButton("cancel",
+                     label = "Cancelar",
+                     width = "10em",
+                     icon = icon("stop-circle"),
+                     class = "btn btn-danger"),
         textOutput("endtext"),
-        HTML("<br><p>Desenvolvido por <a href='https://github.com/vriffel'>Vinicius Riffel</a>.</p>")
+        HTML(credits)
     ) # End mainPanel
 ) # End fluidPage
 
@@ -144,8 +204,16 @@ server <- function(input, output) {
             }) # End renderText
         }) # End observeEvent
 
+    # Cancela a execução da aplicação.
     observeEvent(
-        eventExpr = input$end,
+        eventExpr = input$cancel,
+        handlerExpr = {
+            stopApp()
+        }
+    )
+
+    observeEvent(
+        eventExpr = input$submit,
         handlerExpr = {
             endtime <- Sys.time()
             timediff <- as.numeric(difftime(endtime,
@@ -159,10 +227,14 @@ server <- function(input, output) {
             output$text <- renderText({
                 NULL
             })
+            cat(cnt, "\n", file = "counter.txt")
             returnvalue <- c(
                 "session_id" = substr(basename(tempfile()), 6, 20),
                 "text_id" = smpread,
                 style_vec,
+                order = i,
+                cell = u[i],
+                ts = as.integer(Sys.time()),
                 "time_elapsed" = timediff)
             cat(paste(returnvalue, collapse = ";"),
                 "\n",
--- estilo-texto/orientacoes.md
+++ estilo-texto/orientacoes.md
@@ -5,6 +5,8 @@ Um texto será apresentado com uma dada formatação para que o tempo de
 leitura seja observado. Pressione **Começar** para iniciar a leitura do
 texto. Faça a leitura de forma a compreender o texto e pronunciar
 mentalmente cada palavra. Ao final da leitura, pressione imediatamente
-**Concluir** para gravar o tempo de leitura.
+**Submeter** para gravar o tempo de leitura. Caso não queira submeter ou
+algo tenha comprometido a sua leitura, pressione **Cancelar**. Acesse a
+aplicação novamente para recomeçar.
 
 Os desenvolvedores agradecem sua colaboração.
